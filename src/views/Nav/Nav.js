1fba5d25a3cef390b51549cf5fdcac703d6aaa90
Phan Thac <phanthac.futa@gmail.com>
2023-12-24 09:05:08 +0700

    update code NavLink tag

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Nav.scss"
+import { Link, NavLink } from 'react-router-dom';
+
 
 class Nav extends React.Component {
     render() {
         return (
             <div className="topnav">
-                <a className="active" href="/">Home</a>
-                <a href="/todos">Todos</a>
-                <a href="/about">About</a>
+                <NavLink activeClassName="active" to="/" exact={true}>Home</NavLink>
+                <NavLink activeClassName="active" to="/todos">Todos</NavLink>
+                <NavLink activeClassName="active" to="/about">About</NavLink>
             </div>
         )
     }
